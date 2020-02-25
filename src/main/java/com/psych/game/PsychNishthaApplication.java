295a5955c13f0cb00ad2b760da591b4cd31dc616
nishtha95 <paliwal.nishtha@gmail.com>
2020-02-26 02:09:29 +0530

    init

---
+++ src/main/java/com/psych/game/PsychNishthaApplication.java
@@ -0,0 +1,13 @@
+package com.psych.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychNishthaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychNishthaApplication.class, args);
+	}
+
+}
